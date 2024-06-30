10fd322707c701aaf10e515aa63a31b4002628a2
Vladyslav Danchuk <122180913+danchuk-v@users.noreply.github.com>
2024-06-30 16:00:00 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $('.header-menu-hamburger').click(function(event) {
+        $('.header-menu-hamburger, .header-menu-list').toggleClass('active');
+   });
+ });
+
+ 
